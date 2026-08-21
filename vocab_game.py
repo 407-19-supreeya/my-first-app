2e54c7be12c7c75e22c92a65514866c29e4acc33
407-19-supreeya <54395@yupparaj.ac.th>
2026-08-21 12:20:01 +0700

    Update vocab_game.py

--- vocab_game.py
+++ vocab_game.py
@@ -13,14 +13,14 @@ if "ans4_val" not in st.session_state:
     st.session_state.ans4_val = ""
 
 
-
 def reset_game():
-    st.session_state.ans1_val = "" # เคลียร์ค่าช่องข้อ 1
-    st.session_state.ans2_val = "" # เคลียร์ค่าช่องข้อ 2
-    st.session_state.ans3_val = "" # เคลียร์ค่าช่องข้อ 3
-    st.session_state.ans4_val = "" # เคลียร์ค่าช่องข้อ 4
-    st.session_state.start = time.time() # เริ่มเวลาใหม่
-    st.session_state.is_ended = False # ปิด Dialog
+    st.session_state.ans1_val = ""  # เคลียร์ค่าช่องข้อ 1
+    st.session_state.ans2_val = ""  # เคลียร์ค่าช่องข้อ 2
+    st.session_state.ans3_val = ""  # เคลียร์ค่าช่องข้อ 3
+    st.session_state.ans4_val = ""  # เคลียร์ค่าช่องข้อ 4
+    st.session_state.start = time.time()  # เริ่มเวลาใหม่
+    st.session_state.is_ended = False  # ปิด Dialog
+
 
 @st.dialog("📊 สรุปผลการเล่นเกม")
 def show_result_dialog(ans1, ans2, ans3, ans4):
@@ -31,39 +31,37 @@ def show_result_dialog(ans1, ans2, ans3, ans4):
     u_ans2 = ans2.strip().lower()
     u_ans3 = ans3.strip().lower()
     u_ans4 = ans4.strip().lower()
-  
+
     if u_ans1 == "apple":
         st.success("✅ ข้อ 1: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 1: ยังไม่ถูกต้อง (คุณตอบ '{u_ans1}')")
 
- 
     if u_ans2 == "fish":
         st.success("✅ ข้อ 2: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 2: ยังไม่ถูกต้อง (คุณตอบ '{u_ans2}')")
 
- 
     if u_ans3 == "watermelon":
         st.success("✅ ข้อ 3: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 3: ยังไม่ถูกต้อง (คุณตอบ '{u_ans3}')")
 
- 
     if u_ans4 == "avocado":
         st.success("✅ ข้อ 4: ถูกต้อง")
         score += 1
     else:
         st.error(f"❌ ข้อ 4: ยังไม่ถูกต้อง (คุณตอบ '{u_ans4}')")
+
     st.info(f"🏅 ได้คะแนนรวม: {score} คะแนน")
 
     if score == 2:
-       st.success("🎉 You win!")
+        st.success("🎉 You win!")
     else:
-       st.error("💀 You lose!")
+        st.error("💀 You lose!")
 
 
 st.button(" เริ่มเล่นเกม", on_click=reset_game)
@@ -79,29 +77,28 @@ if "start" in st.session_state and not st.session_state.get("is_ended", False):
 
 st.divider()
 
-
 ans1 = st.text_input(
-     "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
-     value=st.session_state.ans1_val,
+    "ข้อ 1: An `a _ _ l e` a day keeps the doctor away. 🍎",
+    value=st.session_state.ans1_val,
 )
 ans2 = st.text_input(
-     "ข้อ 2: Cats love to eat `f _ s h` . 🐟",
-     value=st.session_state.ans2_val,
+    "ข้อ 2: Cats love to eat `f _ s h` . 🐟",
+    value=st.session_state.ans2_val,
 )
 ans3 = st.text_input(
-     "ข้อ 3: An `w _ t e _ _ e l _ n` green outside,red inside. 🍉",
-     value=st.session_state.ans3_val,
+    "ข้อ 3: An `w _ t e _ _ e l _ n` green outside,red inside. 🍉",
+    value=st.session_state.ans3_val,
 )
 ans4 = st.text_input(
-     "ข้อ 4: green and creamy `_ v o _ a _ o` . 🥑",
-     value=st.session_state.ans4_val,
+    "ข้อ 4: green and creamy `_ v o _ a _ o` . 🥑",
+    value=st.session_state.ans4_val,
 )
 st.session_state.ans1_val = ans1
 st.session_state.ans2_val = ans2
 st.session_state.ans3_val = ans3
 st.session_state.ans4_val = ans4
 
-  
+
 if "start" in st.session_state and not st.session_state.get("is_ended", False):
     if st.button("ส่งคำตอบ"):
         st.session_state.is_ended = True
